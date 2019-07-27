d67257001fa7c5fd37e18efbff881d6951e42905
Sahil Verma <me.sahilverma@hotmail.com>
2019-07-27 13:20:59 +0530

    Jwt Bearer Token Authentication with Login Method is implemented and ready to use.

--- FriendsCoolWater/Controllers/AccountController.cs
+++ FriendsCoolWater/Controllers/AccountController.cs
@@ -1,8 +1,15 @@
-﻿using FriendsCoolWater.Models;
+﻿using FriendsCoolWater.Helpers;
+using FriendsCoolWater.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace FriendsCoolWater.Controllers
@@ -12,29 +19,33 @@ namespace FriendsCoolWater.Controllers
     {
         private UserManager<IdentityUser> _userManager { get; set; }
         private SignInManager<IdentityUser> _signInManager { get; set; }
+        public object JwtRegisteredClaimName { get; private set; }
 
-        public AccountController(
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager)
+        private readonly AppSettings _appSettings;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            IOptions<AppSettings> appSettings)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _appSettings = appSettings.Value;
         }
 
         [HttpPost("action")]
-        public async Task<IActionResult> Register([FromBody]RegisterViewModel register)
+        public async Task<IActionResult> Register([FromBody]RegisterViewModel formData)
         {
             // Will hold all the errors
             List<string> errorList = new List<string>();
 
             var user = new IdentityUser
             {
-                Email = register.Email,
-                UserName = register.Username,
+                Email = formData.Email,
+                UserName = formData.Username,
                 SecurityStamp = Guid.NewGuid().ToString() // Look for any kind for changes in the model
             };
 
-            var result = await _userManager.CreateAsync(user, password: register.Password);
+            var result = await _userManager.CreateAsync(user, password: formData.Password);
             if (result.Succeeded)
             {
                 // Add new user as Customer Role always
@@ -54,6 +65,50 @@ namespace FriendsCoolWater.Controllers
             }
 
             return BadRequest(new JsonResult(errorList));
+        }
+
+        [HttpPost("action")]
+        public async Task<IActionResult> Login([FromBody]LoginViewModel formData)
+        {
+            var user = await _userManager.FindByNameAsync(formData.Username);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, formData.Password))
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_appSettings.Secret));
+                var tokenExpiryTime = Convert.ToDouble(_appSettings.ExpireTime);
+                // Confirmation Email :: Do that later
+
+                // Generate JWT Token
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[] {
+                        new Claim( JwtRegisteredClaimNames.Sub, formData.Username),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(ClaimTypes.NameIdentifier, user.Id),
+                        new Claim(ClaimTypes.Role, roles.FirstOrDefault()),
+                        new Claim("LoggedOn", DateTime.Now.ToString())
+                    }),
+                    SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature),
+                    Issuer = _appSettings.Site,
+                    Audience = _appSettings.Audience,
+                    Expires = DateTime.UtcNow.AddMinutes(tokenExpiryTime)
+
+                };
+
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                return Ok(new
+                {
+                    token = tokenHandler.WriteToken(token),
+                    expiration = token.ValidTo,
+                    username = user.UserName,
+                    userRole = roles.FirstOrDefault()
+                });
+            }
+
+            ModelState.AddModelError("", "Username/Password was not found");
+            return Unauthorized(new { LoginError = "Please Check Login Credentials - Invalid Username/Password was entered" });
         }
     }
 }
