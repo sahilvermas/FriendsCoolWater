726e862d913ecc0812dace6ef41f0cf939e0d156
Sahil Verma <me.sahilverma@hotmail.com>
2019-07-27 14:40:02 +0530

    Team Method implemented.

---
+++ FriendsCoolWater/Controllers/TeamController.cs
@@ -0,0 +1,88 @@
+﻿using FriendsCoolWater.Data;
+using FriendsCoolWater.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FriendsCoolWater.Controllers
+{
+    [Route("api/[controller]")]
+    public class TeamController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public TeamController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult GetTeams()
+        {
+            return Ok(_db.Teams.ToList());
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult GetTeam([FromRoute]int id)
+        {
+            return Ok(_db.Teams.Where(t => t.Id == id).ToList());
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> AddTeam([FromBody]TeamModel formData)
+        {
+            var newTeam = new TeamModel()
+            {
+                Id = formData.Id,
+                Name = formData.Name,
+                Active = formData.Active,
+                Description = formData.Description
+            };
+
+            await _db.Teams.AddAsync(newTeam);
+            await _db.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> UpdateTeam([FromRoute]int id, [FromBody]TeamModel formData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var team = _db.Teams.FirstOrDefault(t => t.Id == id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            team.Name = formData.Name;
+            team.Description = formData.Description;
+            team.Active = formData.Active;
+
+            _db.Entry(team).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            await _db.SaveChangesAsync();
+
+            return Ok(new JsonResult($"The Team with Id {id} updated sucessfully."));
+        }
+
+        [HttpDelete("[action]")]
+        public async Task<IActionResult> DeleteTeam([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var team = await _db.Teams.FindAsync(id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            _db.Teams.Remove(team);
+            await _db.SaveChangesAsync();
+            return Ok(new JsonResult($"Team with Id {id} deleted sucessfully."));
+        }
+    }
+}
--- FriendsCoolWater/Models/TeamModel.cs
+++ FriendsCoolWater/Models/TeamModel.cs
@@ -6,7 +6,7 @@ namespace FriendsCoolWater.Models
     public class TeamModel
     {
         [Key]
-        public string Id { get; set; }
+        public int Id { get; set; }
 
         [Required]
         [MaxLength(50)]
